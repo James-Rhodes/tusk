57b9bca112ff99b9e14a1be862d012d23378e40b
James-Rhodes <james.rhodes@hotmail.com.au>
2023-04-25 10:58:32 +1000

    Got the basics of pg dump based syncing working

--- src/actions/sync/syncers/mod.rs
+++ src/actions/sync/syncers/mod.rs
@@ -1,6 +1,6 @@
 pub mod function_syncer;
-pub mod table_ddl_syncer;
 pub mod table_data_syncer;
+pub mod table_ddl_syncer;
 
 use std::pin::Pin;
 
@@ -16,7 +16,11 @@ pub trait SQLSyncer {
     fn get_all<'conn>(pool: &'conn PgPool, schema: &'conn str) -> Result<RowStream<'conn>>;
 
     // This returns the DDL from a Postgres query as a stream for writing manually to a file
-    fn get<'conn>(pool: &'conn PgPool, schema: &'conn str, items: &'conn Vec<String>) -> Result<RowStream<'conn>>;
+    fn get<'conn>(
+        pool: &'conn PgPool,
+        schema: &'conn str,
+        items: &'conn Vec<String>,
+    ) -> Result<RowStream<'conn>>;
 }
 
 pub trait PgDumpSyncer {
@@ -25,4 +29,14 @@ pub trait PgDumpSyncer {
 
     // This one will write the ones in items to DDL files
     fn get(schema: &str, items: &Vec<String>) -> Result<()>;
+
+    fn get_ddl_from_bytes<'d>(ddl_bytes: &'d Vec<u8>) -> Result<&'d str> {
+
+        let ddl = std::str::from_utf8(&ddl_bytes)?;
+        let end_of_header_pos = ddl
+            .find("SET")
+            .expect("There should be a SET statement at the start of the DDL");
+
+        return Ok(&ddl[end_of_header_pos..]);
+    }
 }
--- src/actions/sync/syncers/table_ddl_syncer.rs
+++ src/actions/sync/syncers/table_ddl_syncer.rs
@@ -3,11 +3,13 @@ use anyhow::Result;
 use crate::{
     actions::sync::syncers::PgDumpSyncer,
     config_file_manager::{format_config_file, get_uncommented_file_contents},
+    db_manager::get_connection_string,
 };
 
 pub struct TableDDLSyncer {}
 
 impl PgDumpSyncer for TableDDLSyncer {
+
     fn get_all(schema: &str) -> Result<()> {
         let file_path = format!(
             "./.tusk/config/schemas/{}/table_ddl_to_include.conf",
@@ -16,14 +18,42 @@ impl PgDumpSyncer for TableDDLSyncer {
         format_config_file(&file_path)?;
 
         let approved_tables = get_uncommented_file_contents(&file_path)?;
+        
+        // TODO: Make this async as well
+        let parent_dir = format!("./schemas/{}/table_ddl", schema);
+
+        if !std::path::Path::new(&parent_dir).exists() {
+            std::fs::create_dir_all(&parent_dir)?;
+        }
+
+        let db_name = format!("--dbname={}", get_connection_string()?);
+        let schema_arg = format!("--schema={}", schema);
+
+        for table in approved_tables {
+
+            let file_path = format!("{}/{}.sql", &parent_dir, table);
+            let table_arg = format!("--table={}", table);
+
+            let command_out = std::process::Command::new("pg_dump")
+                .args([
+                    &db_name,
+                    "--schema-only",
+                    "--no-owner",
+                    &schema_arg,
+                    &table_arg,
+    //                &file_arg
+                ])
+                .output()?.stdout;
+
+            let ddl = Self::get_ddl_from_bytes(&command_out)?;
+
+            std::fs::write(&file_path, ddl)?;
+        }
 
         return Ok(());
     }
 
-    fn get(
-        schema: &str,
-        items: &Vec<String>,
-    ) -> Result<()> {
+    fn get(schema: &str, items: &Vec<String>) -> Result<()> {
         // TODO: See if this can be changed to have no clones/collects
         let items = items
             .iter()
