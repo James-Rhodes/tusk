ab98a10b046e9ece9a2f83b90c24ec6ee81a700c
James-Rhodes <james.rhodes@hotmail.com.au>
2023-05-31 18:10:50 +1000

    Fixed bug in SQL query

--- src/actions/sync/syncers/function_syncer.rs
+++ src/actions/sync/syncers/function_syncer.rs
@@ -2,9 +2,11 @@ use crate::actions::sync::syncers::SQLSyncer;
 
 const FUNCTION_DDL_QUERY: &str = "
             SELECT
-                format('%I(%s)', p.proname, oidvectortypes(p.proargtypes)) name,
-                pg_get_functiondef(p.oid) definition,
-                format('functions/%I/%I(%s)', p.proname, p.proname, oidvectortypes(p.proargtypes)) file_path
+                format('%I(%s)', 
+                p.proname, 
+                oidvectortypes(p.proargtypes)) AS name,
+                pg_get_functiondef(p.oid) AS definition,
+                format('functions/%I/%I(%s)', p.proname, p.proname, oidvectortypes(p.proargtypes)) AS file_path
             FROM pg_proc p INNER JOIN pg_namespace ns ON (p.pronamespace = ns.oid)
             WHERE ns.nspname = $1
             AND p.proname IN (SELECT * FROM UNNEST($2))
