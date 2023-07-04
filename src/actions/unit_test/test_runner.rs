192834243d9dcaed4afbff3540cc435f5a28fc84
James-Rhodes <james.rhodes@hotmail.com.au>
2023-07-05 07:15:05 +1000

    Made test information a enum struct to allow for test names

--- src/actions/unit_test/test_runner.rs
+++ src/actions/unit_test/test_runner.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
 use crate::{
-    actions::unit_test::test_config_manager::{TestConfig, get_test_config},
-    db_manager::error_handling::get_db_error
+    actions::unit_test::test_config_manager::{get_test_config, TestConfig},
+    db_manager::error_handling::get_db_error,
 };
 use anyhow::{bail, Result};
 use futures::TryStreamExt;
@@ -12,8 +12,13 @@ use super::test_config_manager::TestSideEffectConfig;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum TestResult {
-    Passed,
-    Failed(String),
+    Passed {
+        test_name: String,
+    },
+    Failed {
+        test_name: String,
+        error_message: String,
+    },
 }
 
 // runs a test given a unit test definition
@@ -51,20 +56,46 @@ impl TestRunner {
         let query: &str = &test.query;
 
         let func_output_result = match &test.expected_output {
-            Some(expected) => self.check_query_results(pool, query, &expected, "Query Result").await?,
+            Some(expected) => {
+                self.check_query_results(pool, query, &expected, &test.name, "Query Result")
+                    .await?
+            }
             None => match pool.execute(query).await {
-                Ok(_) => TestResult::Passed,
-                Err(e) => TestResult::Failed(get_db_error(e)),
+                Ok(_) => TestResult::Passed {
+                    test_name: test.name.clone(),
+                },
+                Err(e) => TestResult::Failed {
+                    test_name: test.name.clone(),
+                    error_message: get_db_error(e),
+                },
             },
         };
 
-        if let TestResult::Failed(_) = func_output_result {
+        if let TestResult::Failed {
+            test_name: _,
+            error_message: _,
+        } = func_output_result
+        {
             return Ok(func_output_result);
         }
 
         let func_side_effect_result = match &test.expected_side_effect {
-            Some(TestSideEffectConfig{table_query, expected_query_results}) => self.check_query_results(pool, table_query, expected_query_results, "Side Effect").await?,
-            None => TestResult::Passed,
+            Some(TestSideEffectConfig {
+                table_query,
+                expected_query_results,
+            }) => {
+                self.check_query_results(
+                    pool,
+                    table_query,
+                    expected_query_results,
+                    &test.name,
+                    "Side Effect",
+                )
+                .await?
+            }
+            None => TestResult::Passed {
+                test_name: test.name.clone(),
+            },
         };
 
         return Ok(func_side_effect_result);
@@ -75,7 +106,8 @@ impl TestRunner {
         pool: &PgPool,
         query: &str,
         expected_result: &Vec<HashMap<String, String>>,
-        test_prefix: &str
+        test_name: &str,
+        test_prefix: &str,
     ) -> Result<TestResult> {
         let mut rows = pool.fetch(query);
 
@@ -84,7 +116,10 @@ impl TestRunner {
         while let Some(row) = match rows.try_next().await {
             Ok(row_op) => row_op,
             Err(e) => {
-                return Ok(TestResult::Failed(get_db_error(e)));
+                return Ok(TestResult::Failed {
+                    test_name: test_name.to_string(),
+                    error_message: get_db_error(e),
+                });
             }
         } {
             if current_row_index >= expected_result.len() {
@@ -96,20 +131,25 @@ impl TestRunner {
             let row_map = Self::row_to_map(row)?;
 
             if row_map != expected_result[current_row_index] {
-                return Ok(TestResult::Failed(format!(
-                    "{}: Returned row was not equal:\nExpected {:?}, Received: {:?}",
-                    test_prefix, expected_result[current_row_index], row_map
-                )));
+                return Ok(TestResult::Failed {
+                    test_name: test_name.to_string(),
+                    error_message: format!(
+                        "{}: Returned row was not equal:\nExpected {:?}, Received: {:?}",
+                        test_prefix, expected_result[current_row_index], row_map
+                    ),
+                });
             }
             current_row_index += 1;
         }
 
         if current_row_index != expected_result.len() {
             // Double check the above
-            return Ok(TestResult::Failed(format!("{}: The number of returned rows from query: {} was incorrect.\nReceived {} rows, expected {}", test_prefix, query, current_row_index, expected_result.len())));
+            return Ok(TestResult::Failed{test_name: test_name.to_string(), error_message: format!("{}: The number of returned rows from query: {} was incorrect.\nReceived {} rows, expected {}", test_prefix, query, current_row_index, expected_result.len()) });
         }
 
-        return Ok(TestResult::Passed);
+        return Ok(TestResult::Passed {
+            test_name: test_name.to_string(),
+        });
     }
 
     fn row_to_map(row: PgRow) -> Result<HashMap<String, String>> {
@@ -183,22 +223,40 @@ mod tests {
         let pool = db_connection.get_connection_pool();
         let results = tokio_test::block_on(test_runner.run_tests(&pool)).expect("This to not fail");
 
-        assert_eq!(results[0], TestResult::Passed);
+        assert_eq!(
+            results[0],
+            TestResult::Passed {
+                test_name: "Passing Test".to_string()
+            }
+        );
         assert_eq!(
             results[1],
-            TestResult::Failed(format!(
-                "Query Result: Returned row was not equal:\nExpected {:?}, Received: {:?}",
-                HashMap::from([("the_sum", "123")]),
-                HashMap::from([("the_sum", "15")])
-            ))
+            TestResult::Failed {
+                test_name: "Failing Test".to_string(),
+                error_message: format!(
+                    "Query Result: Returned row was not equal:\nExpected {:?}, Received: {:?}",
+                    HashMap::from([("the_sum", "123")]),
+                    HashMap::from([("the_sum", "15")])
+                )
+            }
         );
-        assert_eq!(results[2], TestResult::Failed(format!("Query Result: The number of returned rows from query: {} was incorrect.\nReceived 1 rows, expected 2", "SELECT SUM(nums) AS the_sum FROM UNNEST(ARRAY[1,2,3,4,5]) nums;")));
+        assert_eq!(results[2], TestResult::Failed{ test_name: "Too few returned rows".to_string(), error_message: format!("Query Result: The number of returned rows from query: {} was incorrect.\nReceived 1 rows, expected 2", "SELECT SUM(nums) AS the_sum FROM UNNEST(ARRAY[1,2,3,4,5]) nums;") });
 
-        assert_eq!(results[3], TestResult::Failed(format!("Query Result: The number of returned rows from query: {} was incorrect.\nReceived 3 rows, expected 1", "SELECT UNNEST(ARRAY[1,2,3]) some_nums;")));
+        assert_eq!(results[3], TestResult::Failed{test_name: "Too many returned rows".to_string(), error_message: format!("Query Result: The number of returned rows from query: {} was incorrect.\nReceived 3 rows, expected 1", "SELECT UNNEST(ARRAY[1,2,3]) some_nums;") });
 
-        assert_eq!(results[4], TestResult::Passed);
+        assert_eq!(
+            results[4],
+            TestResult::Passed {
+                test_name: "Checking multiple correct rows work".to_string()
+            }
+        );
 
-        assert_eq!(results[5], TestResult::Passed);
+        assert_eq!(
+            results[5],
+            TestResult::Passed {
+                test_name: "Checking multiple correct rows and columns work".to_string()
+            }
+        );
     }
 
     #[test]
@@ -206,8 +264,8 @@ mod tests {
         let db_connection = tokio_test::block_on(DbConnection::new()).unwrap();
         let pool = db_connection.get_connection_pool();
         tokio_test::block_on(pool.execute("DROP TABLE IF EXISTS public.tusk_test;")).unwrap();
-        tokio_test::block_on(pool.execute("CREATE TABLE public.tusk_test(name TEXT, num BIGINT);")).unwrap();
-
+        tokio_test::block_on(pool.execute("CREATE TABLE public.tusk_test(name TEXT, num BIGINT);"))
+            .unwrap();
 
         let test_config_text = r#"
 
@@ -261,14 +319,29 @@ mod tests {
 
         let results = tokio_test::block_on(test_runner.run_tests(&pool)).expect("This to not fail");
 
-        assert_eq!(results[0], TestResult::Passed);
-        assert_eq!(results[1], TestResult::Failed(format!("Side Effect: The number of returned rows from query: {} was incorrect.\nReceived 2 rows, expected 3", "SELECT name FROM public.tusk_test WHERE name = 'Bar';")));
+        assert_eq!(
+            results[0],
+            TestResult::Passed {
+                test_name: "Passing Test".to_string()
+            }
+        );
+        assert_eq!(results[1], TestResult::Failed{test_name: "Too few returned rows".to_string(), error_message: format!("Side Effect: The number of returned rows from query: {} was incorrect.\nReceived 2 rows, expected 3", "SELECT name FROM public.tusk_test WHERE name = 'Bar';")});
 
-        assert_eq!(results[2], TestResult::Failed(format!("Side Effect: The number of returned rows from query: {} was incorrect.\nReceived 2 rows, expected 1", "SELECT name FROM public.tusk_test WHERE name = 'Baz';")));
+        assert_eq!(results[2], TestResult::Failed{test_name: "Too many returned rows".to_string(), error_message: format!("Side Effect: The number of returned rows from query: {} was incorrect.\nReceived 2 rows, expected 1", "SELECT name FROM public.tusk_test WHERE name = 'Baz';")});
 
-        assert_eq!(results[3], TestResult::Passed);
+        assert_eq!(
+            results[3],
+            TestResult::Passed {
+                test_name: "Multiple Correct Rows".to_string()
+            }
+        );
 
-        assert_eq!(results[4], TestResult::Passed);
+        assert_eq!(
+            results[4],
+            TestResult::Passed {
+                test_name: "Multiple Correct Rows and Columns".to_string()
+            }
+        );
 
         tokio_test::block_on(pool.execute("DROP TABLE IF EXISTS public.tusk_test;")).unwrap();
     }
